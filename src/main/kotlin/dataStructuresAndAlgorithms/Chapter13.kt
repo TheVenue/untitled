2f7439cd1948647a889f5ee7288e1aa45cf573c3
Marvin D.C. Patrick <marvindcpatrick@gmail.com>
2021-10-18 21:39:20 -0400

    Setting up the first problem.

--- src/main/kotlin/dataStructuresAndAlgorithms/Chapter13.kt
+++ src/main/kotlin/dataStructuresAndAlgorithms/Chapter13.kt
@@ -4,3 +4,11 @@ private fun main() {
 
 }
 
+/* Given an array of positive numbers, return the greatest product
+ * of three numbers. Use sorting to implement the function in a way
+ * that it computes at O(N log N)
+ */
+private fun sortAndReturnGreatestProduct(listOfNumbers: MutableList<Int>): Int {
+    return -1
+}
+
