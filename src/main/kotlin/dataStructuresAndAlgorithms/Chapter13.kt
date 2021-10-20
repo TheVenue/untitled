d78fa06762ec5200ef044de52cc39231c1ba0296
Marvin D.C. Patrick <marvindcpatrick@gmail.com>
2021-10-19 21:02:30 -0400

    Adding a selection sort.

--- src/main/kotlin/dataStructuresAndAlgorithms/Chapter13.kt
+++ src/main/kotlin/dataStructuresAndAlgorithms/Chapter13.kt
@@ -2,7 +2,14 @@ package dataStructuresAndAlgorithms
 
 private fun main() {
     println(sortAndReturnGreatestProduct(mutableListOf(4, 6, 2, 5, 7, 6, 3, 7, 6, 9)))
-    println(bubbleSort(mutableListOf(4, 6, 2, 5, 7, 6, 3, 7, 6, 9)))
+
+    val bubbleSortedList = mutableListOf(4, 6, 2, 5, 7, 6, 3, 7, 6, 9)
+    bubbleSort(bubbleSortedList)
+    println(bubbleSortedList)
+
+    val selectionSortedList = mutableListOf(4, 6, 2, 5, 7, 6, 3, 7, 6, 9)
+    selectionSort(selectionSortedList)
+    println(selectionSortedList)
 }
 
 private fun sortAndReturnGreatestProduct(listOfNumbers: MutableList<Int>): Int {
@@ -13,7 +20,7 @@ private fun sortAndReturnGreatestProduct(listOfNumbers: MutableList<Int>): Int {
     return largest * secondLargest * thirdLargest
 }
 
-private fun bubbleSort(listOfNumbers: MutableList<Int>) : MutableList<Int> {
+private fun bubbleSort(listOfNumbers: MutableList<Int>) {
     var unsortedUntilIndex = listOfNumbers.size - 1
     var sorted = false
     while (!sorted) {
@@ -33,6 +40,25 @@ private fun bubbleSort(listOfNumbers: MutableList<Int>) : MutableList<Int> {
         }
         unsortedUntilIndex -= 1
     }
-    return listOfNumbers
+}
+
+private fun selectionSort(listOfNumbers: MutableList<Int>) {
+    listOfNumbers.forEachIndexed { i, _ ->
+        var lowestNumberIndex = i
+        var j = i + 1
+
+        while (j < listOfNumbers.size) {
+            if (listOfNumbers[j] < listOfNumbers[lowestNumberIndex]) {
+                lowestNumberIndex = j
+            }
+            j++
+        }
+
+        if (lowestNumberIndex != i) {
+            val temp = listOfNumbers[i]
+            listOfNumbers[i] = listOfNumbers[lowestNumberIndex]
+            listOfNumbers[lowestNumberIndex] = temp
+        }
+    }
 }
 
